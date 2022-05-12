cf4edfe08b86df90234206f7fc7adbb139aedfc0
Jitae9605 <dlwlxo3819@naver.com>
2022-05-12 17:38:59 +0900

    파일 전송 프로그램 작성

---
+++ FUP/Message.cs
@@ -0,0 +1,51 @@
+﻿
+namespace FUP
+{
+	public class CONSTRAINT
+	{
+		public const uint REQ_FILE_SEND =	0x01;
+		public const uint REP_FILE_SEND =	0x02;
+		public const uint FILE_SEND_DATA =	0x03;
+		public const uint FILE_SEND_RES =	0x04;
+
+		public const byte NOT_FRAGMENTED = 0x00;
+		public const byte FAGMENTED = 0x01;
+
+		public const byte NOT_LASTING = 0x00;
+		public const byte LASTING = 0x01;
+
+		public const byte ACCEPTED = 0x00;
+		public const byte DENIED = 0x01;
+
+		public const byte FAIL = 0x00;
+		public const byte SUCCESS = 0x01;
+
+
+	}
+
+	public interface ISerializable
+	{
+		byte[] GetBytes();
+		int GetSize();
+	}
+
+	public class Message : ISerializable
+	{
+		public Header Header { get; set; }
+		public ISerializable Body { get; set; }
+
+		public byte[] GetBytes()
+		{
+			byte[] bytes = new byte[GetSize()];
+			Header.GetBytes().CopyTo(bytes, 0);
+			Body.GetBytes().CopyTo(bytes, Header.GetSize());
+
+			return bytes;
+		}
+
+		public int GetSize()
+		{
+			return Header.GetSize() + Body.GetSize();
+		}
+	}
+}
---
+++ FUP/MessageUtil.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace FUP
+{
+	public class MessageUtil
+	{
+		public static void Send(Stream writer, Message msg)
+		{
+			writer.Write(msg.GetBytes(), 0, msg.GetSize());
+		}
+
+		public static Message Receive(Stream reader)
+		{
+			int totalRecv = 0;
+			int sizeToRead = 16;
+			byte[] hbuffer = new byte[sizeToRead];
+
+			while(sizeToRead > 0)
+			{
+				byte[] buffer = new byte[sizeToRead];
+				int recv = reader.Read(buffer, 0, sizeToRead);
+				if (recv == 0)
+					return null;
+
+				buffer.CopyTo(hbuffer, totalRecv);
+				totalRecv += recv;
+				sizeToRead -= recv;
+			}
+
+			Header header = new Header(hbuffer);
+
+			totalRecv = 0;
+			byte[] bBuffer = new byte[header.BODYLEN];
+			sizeToRead = (int)header.BODYLEN;
+
+			while(sizeToRead > 0)
+			{
+				byte[] buffer = new byte[sizeToRead];
+				int recv = reader.Read(buffer, 0, sizeToRead);
+				if (recv == 0)
+					return null;
+
+				buffer.CopyTo(bBuffer, totalRecv);
+				totalRecv += recv;
+				sizeToRead -= recv;
+			}
+
+			ISerializable body = null;
+			switch(header.MSGTYPE)
+			{
+				case CONSTRAINT.REQ_FILE_SEND:
+					body = new BodyRequest(bBuffer);
+					break;
+				case CONSTRAINT.REP_FILE_SEND:
+					body = new BodyResponse(bBuffer);
+					break;
+				case CONSTRAINT.FILE_SEND_DATA:
+					body = new BodyData(bBuffer);
+					break;
+				case CONSTRAINT.FILE_SEND_RES:
+					body = new BodyResult(bBuffer);
+					break;
+				default:
+					throw new Exception(String.Format("Unkown MSGTYPE : {0}", header.MSGTYPE));
+
+			}
+			return new Message() { Header = header, Body = body };
+		}
+	}
+}
