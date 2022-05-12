2e4493772cf988e9551e5f855d4dea42afca344d
Jitae9605 <dlwlxo3819@naver.com>
2022-05-12 15:04:38 +0900

    주석추가

--- EchoClient/Program.cs
+++ EchoClient/Program.cs
@@ -16,41 +16,47 @@ namespace EchoClient
 				return;
 			}
 
-			string bindIp = args[0];
-			int bindPort = Convert.ToInt32(args[1]);
-			string serverIp = args[2];
-			const int serverPort = 5425;
-			string message = args[3];
+			string bindIp = args[0];                    // 클라이언트 IP					 // 루프백 : 127.0.0.1  =  외부가 아닌 본인 스스로와의 내부통신을 위한 루프IP
+			int bindPort = Convert.ToInt32(args[1]);	// 클라이언트 포트번호			 // 1024 이상을 부여(미만은 Well Known Port로 주요 프로토콜/프로그램등이 사용)
+			string serverIp = args[2];                  // 서버 IP			- 일치해야함	 // 루프백 : 127.0.0.1  =  외부가 아닌 본인 스스로와의 내부통신을 위한 루프IP
+			const int serverPort = 5425;				// 서버의 포트번호	- 일치해야함
+			string message = args[3];					// 서버로 송신할 메세지
 
 			try
 			{
-				IPEndPoint clientAddress = new IPEndPoint(IPAddress.Parse(bindIp), bindPort);
-				IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+				IPEndPoint clientAddress = new IPEndPoint(IPAddress.Parse(bindIp), bindPort);		// 클라이언트IP 및 포트를 통한 클라이언트 주소 생성
+				IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);   // 서버IP 및 포트를 통한 서버 주소 생성
 
 				Console.WriteLine("클라이언트 : {0}, 서버 : {1}",clientAddress.ToString(),serverAddress.ToString());
 
+				// 생서된 주소를 가진 클라이언트 생성
 				TcpClient client = new TcpClient(clientAddress);
 
+				// 서버 주소에 해당하는 서버와 연결시도
 				client.Connect(serverAddress);
 
-				byte[] data = System.Text.Encoding.Default.GetBytes(message);
-
+				// NetworkStream을 생성하고 client에게 Stream소유권?권한?을 부여함
 				NetworkStream stream = client.GetStream();
 
-				stream.Write(data, 0, data.Length);
+				byte[] data = Encoding.Default.GetBytes(message);	 // 송신할 메세지를 byte형으로 인코딩해 data에 저장				 
+				stream.Write(data, 0, data.Length);					 // 생성된 데이터를 넘겨줌
 
+				// 넘겨줬음을 출력
 				Console.WriteLine("송신: {0}", message);
 
+				// data초기화
 				data = new byte[256];
 
 				string responseData = "";
 
+				// NetworkStream을 통해 서버가 응답해 보낸 정보를 읽어서 data에 저장
 				int bytes = stream.Read(data, 0, data.Length);
-				responseData = Encoding.Default.GetString(data, 0, bytes);
-				Console.WriteLine("수신: {0}", responseData);
+				responseData = Encoding.Default.GetString(data, 0, bytes);      // 받은 데이터를 responseData에 저장함(분류,보관 목적?)
 
-				stream.Close();
-				client.Close();
+				Console.WriteLine("수신: {0}", responseData);     // 수신했음을 확인
+
+				stream.Close();			// NetworkStream 닫음
+				client.Close();			// 클라이언트 닫음
 			}
 			catch (SocketException e)
 			{
@@ -58,8 +64,6 @@ namespace EchoClient
 			}
 
 			Console.WriteLine("클라이언트를 종료합니다.");
-			
-
 		}
 	}
 }
--- EchoServer/Program.cs
+++ EchoServer/Program.cs
@@ -10,49 +10,57 @@ namespace EchoServer
 	{
 		static void Main(string[] args)
 		{
+			// args없으면 사용법 소개
 			if(args.Length < 1)
 			{
-				Console.WriteLine("사용법 : {0} <Bind IP>", Process.GetCurrentProcess().ProcessName);
+				Console.WriteLine("사용법 : {0} <Bind IP>", Process.GetCurrentProcess().ProcessName);	
 				return;
 			}
 
-			string BindIP = args[0];
-			const int bindPort = 5425;
-			TcpListener server = null;
+			// args있으면 서버 생성
+			string BindIP = args[0];		// 서버 IP			// 루프백 : 127.0.0.1  =  외부가 아닌 본인 스스로와의 내부통신을 위한 루프IP
+			const int bindPort = 5425;		// 서버포트번호
+			TcpListener server = null;		// TCP소켓서버 생성
 			try
 			{
 				IPEndPoint localAddress = new IPEndPoint(IPAddress.Parse(BindIP), bindPort);
 
+				// 생성된 서버에 IP 및 포트부여
 				server = new TcpListener(localAddress);
 
+				// 서버 동작시작
 				server.Start();
 
 				Console.WriteLine("메아리 서버 시작...");
 
+				// 클라이언트의 응답/요청 대기
 				while(true)
 				{
+					// 클라이언트의 접속이 있으면 client가 생성되고 server는 접속을 승인한다(현재는 별도 보안없음)
 					TcpClient client = server.AcceptTcpClient();
 					Console.WriteLine("클라이언트 접속 : {0}", ((IPEndPoint)client.Client.RemoteEndPoint).ToString());
 
+					// NetworkStream을 생성하고 client에게 Stream소유권?권한?을 부여함
 					NetworkStream stream = client.GetStream();
 
 					int length;
 					string data = null;
 					byte[] bytes = new byte[256];
 
-					while((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+					// NetworkStream의 남은 데이터를 읽어와 byte에 저장한다(이게 0byte일때까지 반복)
+					while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
 					{
-						data = Encoding.Default.GetString(bytes, 0, length);
-						Console.WriteLine(String.Format("수신: {0}", data));
+						data = Encoding.Default.GetString(bytes, 0, length);	// byte형으로 bytes에 저장된 내용을 data에 string형으로 인코딩해 저장
+						Console.WriteLine(String.Format("수신: {0}", data));		
+						
+						byte[] msg = Encoding.Default.GetBytes(data);			// string형으로 data에 저장된 내용을 msg에 byte형으로 인코팅해 저장
 
-						byte[] msg = Encoding.Default.GetBytes(data);
-
-						stream.Write(msg, 0, msg.Length);
-						Console.WriteLine(String.Format("송신: {0}", data));
+						stream.Write(msg, 0, msg.Length);                       // NetworkStream에 msg를 넘겨줌
+						Console.WriteLine(String.Format("송신: {0}", data));		// data를 보냈음을 출력
 					}
 
-					stream.Close();
-					client.Close();
+					stream.Close();         // NetworkStream 닫음
+					client.Close();			// 클라이언트 닫음
 				}
 			}
 
