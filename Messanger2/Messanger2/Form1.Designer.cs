e8bade94b53329f85f9a10cb6869d7385e721505
Jitae9605 <dlwlxo3819@naver.com>
2022-06-15 16:31:57 +0900

    TCP 소켓 통신을 이용한 다중 메신저 프로그램

---
+++ Messanger2/Messanger2/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace Messanger2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.tsbtnOption = new System.Windows.Forms.ToolStripDropDownButton();
+			this.설정ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.닫기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.tsbtnConn = new System.Windows.Forms.ToolStripButton();
+			this.tsbtnDisconn = new System.Windows.Forms.ToolStripButton();
+			this.plOption = new System.Windows.Forms.Panel();
+			this.btnClose = new System.Windows.Forms.Button();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.cbServer = new System.Windows.Forms.CheckBox();
+			this.txtPort = new System.Windows.Forms.TextBox();
+			this.txtId = new System.Windows.Forms.TextBox();
+			this.txtIp = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.txtMessage = new System.Windows.Forms.TextBox();
+			this.btnSender = new System.Windows.Forms.Button();
+			this.rtbText = new System.Windows.Forms.RichTextBox();
+			this.toolStrip1.SuspendLayout();
+			this.plOption.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnOption,
+            this.tsbtnConn,
+            this.tsbtnDisconn});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(357, 25);
+			this.toolStrip1.TabIndex = 0;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// tsbtnOption
+			// 
+			this.tsbtnOption.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.tsbtnOption.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.설정ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.닫기ToolStripMenuItem});
+			this.tsbtnOption.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnOption.Image")));
+			this.tsbtnOption.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.tsbtnOption.Name = "tsbtnOption";
+			this.tsbtnOption.Size = new System.Drawing.Size(29, 22);
+			this.tsbtnOption.Text = "toolStripDropDownButton1";
+			this.tsbtnOption.ToolTipText = "환경설정";
+			// 
+			// 설정ToolStripMenuItem
+			// 
+			this.설정ToolStripMenuItem.Name = "설정ToolStripMenuItem";
+			this.설정ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+			this.설정ToolStripMenuItem.Text = "설정";
+			this.설정ToolStripMenuItem.Click += new System.EventHandler(this.설정ToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(95, 6);
+			// 
+			// 닫기ToolStripMenuItem
+			// 
+			this.닫기ToolStripMenuItem.Name = "닫기ToolStripMenuItem";
+			this.닫기ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+			this.닫기ToolStripMenuItem.Text = "닫기";
+			this.닫기ToolStripMenuItem.Click += new System.EventHandler(this.닫기ToolStripMenuItem_Click);
+			// 
+			// tsbtnConn
+			// 
+			this.tsbtnConn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.tsbtnConn.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnConn.Image")));
+			this.tsbtnConn.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.tsbtnConn.Name = "tsbtnConn";
+			this.tsbtnConn.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnConn.Text = "연결";
+			this.tsbtnConn.Click += new System.EventHandler(this.tsbtnConn_Click);
+			// 
+			// tsbtnDisconn
+			// 
+			this.tsbtnDisconn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.tsbtnDisconn.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnDisconn.Image")));
+			this.tsbtnDisconn.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.tsbtnDisconn.Name = "tsbtnDisconn";
+			this.tsbtnDisconn.Size = new System.Drawing.Size(23, 22);
+			this.tsbtnDisconn.Text = "연결끊기";
+			this.tsbtnDisconn.Click += new System.EventHandler(this.tsbtnDisconn_Click);
+			// 
+			// plOption
+			// 
+			this.plOption.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.plOption.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.plOption.Controls.Add(this.btnClose);
+			this.plOption.Controls.Add(this.btnSave);
+			this.plOption.Controls.Add(this.cbServer);
+			this.plOption.Controls.Add(this.txtPort);
+			this.plOption.Controls.Add(this.txtId);
+			this.plOption.Controls.Add(this.txtIp);
+			this.plOption.Controls.Add(this.label4);
+			this.plOption.Controls.Add(this.label3);
+			this.plOption.Controls.Add(this.label2);
+			this.plOption.Location = new System.Drawing.Point(16, 57);
+			this.plOption.Name = "plOption";
+			this.plOption.Size = new System.Drawing.Size(327, 210);
+			this.plOption.TabIndex = 1;
+			// 
+			// btnClose
+			// 
+			this.btnClose.Location = new System.Drawing.Point(174, 140);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(80, 40);
+			this.btnClose.TabIndex = 5;
+			this.btnClose.Text = "닫기";
+			this.btnClose.UseVisualStyleBackColor = true;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(71, 140);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(80, 40);
+			this.btnSave.TabIndex = 5;
+			this.btnSave.Text = "설정";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// cbServer
+			// 
+			this.cbServer.AutoSize = true;
+			this.cbServer.Location = new System.Drawing.Point(32, 118);
+			this.cbServer.Name = "cbServer";
+			this.cbServer.Size = new System.Drawing.Size(72, 16);
+			this.cbServer.TabIndex = 4;
+			this.cbServer.Text = "서버실행";
+			this.cbServer.UseVisualStyleBackColor = true;
+			this.cbServer.CheckedChanged += new System.EventHandler(this.cbServer_CheckedChanged);
+			// 
+			// txtPort
+			// 
+			this.txtPort.BackColor = System.Drawing.SystemColors.Control;
+			this.txtPort.Location = new System.Drawing.Point(97, 91);
+			this.txtPort.Name = "txtPort";
+			this.txtPort.Size = new System.Drawing.Size(192, 21);
+			this.txtPort.TabIndex = 3;
+			// 
+			// txtId
+			// 
+			this.txtId.Location = new System.Drawing.Point(97, 64);
+			this.txtId.Name = "txtId";
+			this.txtId.Size = new System.Drawing.Size(192, 21);
+			this.txtId.TabIndex = 3;
+			// 
+			// txtIp
+			// 
+			this.txtIp.Location = new System.Drawing.Point(97, 37);
+			this.txtIp.Name = "txtIp";
+			this.txtIp.Size = new System.Drawing.Size(192, 21);
+			this.txtIp.TabIndex = 3;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(30, 94);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(38, 12);
+			this.label4.TabIndex = 2;
+			this.label4.Text = "PORT";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(30, 67);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(16, 12);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "ID";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(30, 40);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(16, 12);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "IP";
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.LightSkyBlue;
+			this.panel2.Controls.Add(this.panel3);
+			this.panel2.Controls.Add(this.btnSender);
+			this.panel2.Location = new System.Drawing.Point(0, 332);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(357, 82);
+			this.panel2.TabIndex = 6;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.SystemColors.Control;
+			this.panel3.Controls.Add(this.txtMessage);
+			this.panel3.Location = new System.Drawing.Point(13, 12);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(214, 54);
+			this.panel3.TabIndex = 7;
+			// 
+			// txtMessage
+			// 
+			this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.txtMessage.Location = new System.Drawing.Point(13, 12);
+			this.txtMessage.Multiline = true;
+			this.txtMessage.Name = "txtMessage";
+			this.txtMessage.Size = new System.Drawing.Size(189, 27);
+			this.txtMessage.TabIndex = 0;
+			this.txtMessage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMessage_KeyPress);
+			// 
+			// btnSender
+			// 
+			this.btnSender.Location = new System.Drawing.Point(233, 12);
+			this.btnSender.Name = "btnSender";
+			this.btnSender.Size = new System.Drawing.Size(110, 54);
+			this.btnSender.TabIndex = 5;
+			this.btnSender.Text = "보내기";
+			this.btnSender.UseVisualStyleBackColor = true;
+			this.btnSender.Click += new System.EventHandler(this.btnSender_Click);
+			// 
+			// rtbText
+			// 
+			this.rtbText.Location = new System.Drawing.Point(0, 28);
+			this.rtbText.Name = "rtbText";
+			this.rtbText.Size = new System.Drawing.Size(357, 298);
+			this.rtbText.TabIndex = 7;
+			this.rtbText.Text = "";
+			this.rtbText.TextChanged += new System.EventHandler(this.rtbText_TextChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(357, 413);
+			this.Controls.Add(this.plOption);
+			this.Controls.Add(this.rtbText);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.toolStrip1);
+			this.Name = "Form1";
+			this.Text = "1:1 채팅";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.plOption.ResumeLayout(false);
+			this.plOption.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripDropDownButton tsbtnOption;
+		private System.Windows.Forms.ToolStripButton tsbtnConn;
+		private System.Windows.Forms.ToolStripButton tsbtnDisconn;
+		private System.Windows.Forms.ToolStripMenuItem 설정ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 닫기ToolStripMenuItem;
+		private System.Windows.Forms.Panel plOption;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.CheckBox cbServer;
+		private System.Windows.Forms.TextBox txtPort;
+		private System.Windows.Forms.TextBox txtId;
+		private System.Windows.Forms.TextBox txtIp;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnClose;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.TextBox txtMessage;
+		private System.Windows.Forms.Button btnSender;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.RichTextBox rtbText;
+	}
+}
+
---
+++ Messanger2/Messanger2/Form1.cs
@@ -0,0 +1,534 @@
+﻿using System;
+using System.Windows.Forms;
+
+using System.Net;   // IP Adress
+using System.Net.Sockets; // Tcp Listen(server, Client) Class
+using System.Threading; // 쓰레드
+using System.IO;    // Stream
+using Microsoft.Win32;  // 레지스트리 클래스 사용. 별칭등의 간단한 정보저장 및 재사용
+
+// 레지스트리 사용 권한을 줘야함
+/*
+	1. 프로젝트 속성에서 보안 -> one .... 체크 -> 완전신뢰
+	2. 생성된 app.manifest의 Level을
+		<requestedExecutionLevel  level="requireAdministrator" uiAccess="false" /> 
+	   로 바꾼다
+	3. 1.의 체크(one...)를 푼다.
+	4. 비주얼스튜디오를 관리자 권한으로 실행해야 경고메시지가 안뜬다.
+ */
+
+
+namespace Messanger2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		
+
+		private TcpListener Server;	// Tcp 통신시 클라이언트의 연결을 수신
+		private TcpClient SerClient, Client ;   //  서버측의 클라이언트, 서버에 접속하려는 클라이언트
+		private NetworkStream mystream; // 네트워크 스트림(데이터 송수신 통로)
+		private StreamReader myRead;    // 읽기 스트림
+		private StreamWriter myWrite;   // 쓰기 스트림
+
+		private Boolean Start = false;	// 서버 기동 flag
+		private Boolean ClientCon = false;  // 클라이언트 기동 flag
+
+		private int myPort;	// 포트번호
+		private string myName;	// 별칭
+		private Thread myReader;    // 전송된 테이터의 읽기 쓰레드
+		private Thread myServer;    // 서버에 접속하려는 클라이언트를 받아 줌
+
+		private RegistryKey Key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\.NETFramwork",true);
+		private Delegate AddText;
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			try
+			{
+				ServerStop();
+				Disconnection();
+			}
+			catch
+			{
+
+				return;
+			}
+			
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// 레지스트리에 별칭이 등록되어 있는지 체크
+			if ((string)Key.GetValue("Message_name") == "")
+			{
+				this.myName = this.txtId.Text;
+				this.myPort = 62000;            // 포트번호는 웰노운포트를 피해서 할것!
+			}
+			else
+			{
+				try
+				{
+					this.myName = (string)Key.GetValue("Message_name");
+					this.myPort = 62000;
+				}
+				catch
+				{
+
+
+				}
+			}
+		}
+
+		private void 설정ToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.설정ToolStripMenuItem.Enabled = false;
+			this.plOption.Visible = true;
+			this.txtId.Focus();
+			this.txtId.Text = (string)Key.GetValue("Message_name"); //별칭
+			this.txtPort.Text = "62001"; // 포트 번호
+		}
+
+		private void 닫기ToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void tsbtnConn_Click(object sender, EventArgs e)
+		{
+			// 서버용 기동인지 클라이언트 기동인지 구분
+			if (this.cbServer.Checked == true) // 서버로 기동
+			{
+				var addr = new IPAddress(0);
+
+				try
+				{
+					this.myName = (string)Key.GetValue("Message_name");
+				}
+				catch
+				{
+					this.myName = this.txtId.Text;
+					this.myPort = Convert.ToInt32(this.txtPort.Text);
+
+				}
+
+				// 서버가 기동상태가 아니면, 서버를 기동
+				if(!this.Start)
+				{
+					try
+					{
+						Server = new TcpListener(addr, this.myPort);
+						Server.Start();
+
+						// 서버 상태 flag를 기동상태로 변경
+						this.Start = true;
+
+						// 화면 컨트롤 제어
+						this.txtMessage.Enabled = true;
+						this.btnSender.Enabled = true;
+						this.txtMessage.Focus();
+						this.tsbtnConn.Enabled = false;
+						this.tsbtnDisconn.Enabled = true;
+						this.cbServer.Enabled = false;
+
+						// 서버에 접속하는 클라이언트를 대기 => 쓰레드
+						myServer = new Thread(ServerStart);
+						myServer.Start();
+
+						this.설정ToolStripMenuItem.Enabled = false;
+
+					}
+					catch
+					{
+						// 접속 에러 Message로 처리
+						Invoke(AddText, "서버를 실행할 수 없습니다.");
+						
+					}
+				}
+				else
+				{
+					// 서버종료처리
+				}
+
+			}
+
+			else // 클라이언트로 기동
+			{
+				// 클라이언트가 기동되지 않았는지 => 기동시킹
+				if(!(this.ClientCon))
+				{
+					// 클라이언트 접속 메소드 호출
+					ClientConnection();
+				}
+				else
+				{
+					this.txtMessage.Enabled = false;
+					this.btnSender.Enabled = false;
+				}
+			}
+		}
+
+		private void ClientConnection()
+		{
+			try
+			{
+				// 클라이온트가 서버에 접속
+				Client = new TcpClient(this.txtIp.Text, this.myPort);
+
+				Invoke(AddText, "서버에 접속했습니다.");
+
+				// 서버와 연결된 통신 통로(스트림) 획득
+				mystream = Client.GetStream();
+
+				myRead = new StreamReader(mystream);
+				myWrite = new StreamWriter(mystream);
+
+				//컨트롤 제어
+				this.ClientCon = true;
+				this.tsbtnConn.Enabled = false;
+				this.tsbtnDisconn.Enabled = true;
+				this.txtMessage.Enabled = true;
+				this.btnSender.Enabled = true;
+				this.txtMessage.Focus();
+
+				// 수신 쓰레드 생성
+				// Receive 메소드는 서버 클라이언트 겸용임.
+				myReader = new Thread(Receive);
+				myReader.Start();
+
+
+
+			}
+			catch
+			{
+				// 서버에 접속 못한 상태를 상태 flag에 반영 후, 메시지로 처리
+				this.ClientCon = false;
+				Invoke(AddText, "서버에 접속하지 못했습니다.");
+			}
+		}
+
+		// 서버 종료 및 자원 해제처리
+		private void ServerStop()
+		{
+			this.Start = false;
+
+			// 컨트롤 제어
+			this.txtMessage.Enabled = false;
+			this.txtMessage.Clear();
+			this.btnSender.Enabled = false;
+			this.tsbtnConn.Enabled = true;
+			this.tsbtnDisconn.Enabled = false;
+			this.cbServer.Enabled = true;
+
+			// 클라이언트 접속상태 flag도 false로 변경
+			this.ClientCon = false;
+
+			// 자원해제
+			// StreadReader 해제
+			if (!(myRead == null))
+			{
+				myRead.Close();
+			}
+			// StreadWriter 해제
+			if (!(myWrite == null))
+			{
+				myWrite.Close();
+			}
+			// Network 스트림 해제
+			if (!(mystream == null))
+			{
+				mystream.Close();
+			}
+
+			// 서버측 TcpClient 해제
+			if (!(SerClient == null))
+			{
+				SerClient.Close();
+			}
+			// TcpListner = 서버 해제
+			if(!(Server == null))
+			{
+				Server.Stop();
+			}
+
+			// 수신쓰레드 해제
+			// 클라이언트 접속담당 쓰레드 해제
+			if (!(myReader == null))
+			{
+				myReader.Join();
+			}
+
+			// 서버와 접속 종료되었음을 메시지로 알림
+			if(!(AddText == null))
+			{
+				Invoke(AddText,"서버와 접속이 끊어졌습니다.");
+			}
+
+		}
+
+		// 클라이언트의 접속 해제 처리
+		private void Disconnection()
+		{
+			this.ClientCon = false;
+
+			try
+			{
+				// 스트림 리더 클래스 해제
+				if(!(myRead == null))
+				{
+					myRead.Close();
+				}
+				// 스트림 쓰는 클래스 해제
+				if(!(myWrite == null))
+				{
+					myWrite.Close();
+				}
+				// 네트워크 스트림 해제
+				if(!(mystream == null))
+				{
+					mystream.Close();
+				}
+				// TcpClient 해제
+				if(!(Client == null))
+				{
+					Client.Close();
+				}
+				// 스레드 종료
+				if(!(myReader == null))
+				{
+					myReader.Join();
+				}
+			}
+			catch
+			{
+
+				return;
+			}
+		}
+
+		// 서버 및 클라이언트가 상대방에게 보내는 메세지를 전송함
+		private void Msg_Send()
+		{
+			try
+			{
+				// 전송 시간정보 획득
+				var dt = Convert.ToString(DateTime.Now);
+
+				// 스트림을 통해서 메세지 전송
+				myWrite.WriteLine(this.myName + "&" + this.txtMessage + "&" + dt);
+				myWrite.Flush();
+
+				// 리치 텍스트박스에 전송메세지 추가
+				MessageView(this.myName + ";" + this.txtMessage.Text);
+				this.txtMessage.Clear();
+			}
+			catch
+			{
+
+				Invoke(AddText, "데이터 전송에 문제가 발생했습니다.");
+				this.txtMessage.Clear();
+			}
+		}
+
+		private void MessageView(string strText)
+		{
+			// 메시지 추가
+			this.rtbText.AppendText(strText + "\r\n");
+			this.rtbText.Focus();
+
+			// 마지막 위치로 이동
+			this.rtbText.ScrollToCaret();
+			this.txtMessage.Focus();
+		}
+
+
+		private void ServerStart()
+		{
+			Invoke(AddText, "서버 실행 : 클라이언트의 접속을 기다립니다.");
+
+			while(Start)
+			{
+				try
+				{
+					// 서버에 접속된 클라이언트
+					SerClient = Server.AcceptTcpClient();
+
+					Invoke(AddText, "클라이언트가 접속되었습니다..");
+
+
+					mystream = SerClient.GetStream();
+
+					myRead = new StreamReader(mystream);
+					myWrite = new StreamWriter(mystream);
+
+					this.ClientCon = true;
+
+					// 클라이언트가 보내온 데이터를 수신하는 쓰레드;
+					myReader = new Thread(Receive);
+					myReader.Start();
+				}
+				catch
+				{
+
+					
+				}
+				
+			}
+		}
+
+		private void Receive()
+		{
+			try
+			{
+				// 클라이언트가 접속 되어 있는 동안 반복
+				while(this.ClientCon)
+				{
+					Thread.Sleep(1);
+					// 스트림으로 부터 읽어올 데이터가 있는지 판단
+					if(mystream.CanRead)
+					{
+						var msg = myRead.ReadLine();
+						var Smsg = msg.Split('&');
+
+						if(Smsg[0] == "S001")
+						{
+							// 전송시각화지만 생략!
+						}
+						else
+						{
+							if(msg.Length > 0)
+							{
+								// 서버 및 클라이언트가 보내온 메세지 출력
+								Invoke(AddText, Smsg[0] + " : " + Smsg[1]);
+							}
+						}
+
+						//  수신데이터 처리
+					}
+				}
+
+			}
+			catch
+			{
+
+				
+			}
+		}
+
+		private void tsbtnDisconn_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void cbServer_CheckedChanged(object sender, EventArgs e)
+		{
+			if (this.cbServer.Checked)
+			{
+				// 서버로 기동시 IP는 내부에서 받아와 사용 => 입력막기
+				this.txtIp.Enabled = false;
+			}
+			else
+			{
+				// 클라이언트로 기동시 IP는 접속할 서버 IP입력
+				this.txtIp.Enabled = true;
+			}
+		}
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			if (this.cbServer.Checked == true) //서버용으로 사용
+			{
+				// 입력된 정보를 레지스트리에 등록
+				ControlCheck();
+			}
+			else // 클라이언트용으로 사용
+			{
+				if (this.txtIp.Text == "")
+				{
+					this.txtIp.Focus();
+				}
+				else
+				{
+					// 입력된 정보를 레지스트리에 등록
+					ControlCheck();
+				}
+			}
+		}
+		private void ControlCheck()
+		{
+			if(this.txtId.Text == "")
+			{
+				this.txtId.Focus();
+			}
+			else if(this.txtPort.Text == "")
+			{
+				this.txtPort.Focus();
+			}
+			else
+			{
+				// 컨트롤러에 입력된 정보를 레지스트리에 등록
+				try
+				{
+					// 레지스트리에 별칭 및 포트 저장
+					var name = this.txtId.Text;
+					var port = this.txtPort.Text;
+					Key.SetValue("Mesaage_name", "name");
+					Key.SetValue("Mesaage_Port", "port");
+
+					// 컨트롤 제어
+					this.plOption.Visible = false;
+					this.설정ToolStripMenuItem.Enabled = true;
+					this.tsbtnConn.Enabled = true;
+				}
+				catch
+				{
+					MessageBox.Show("설정 정보가 저장되지 않았습니다.", "에러",MessageBoxButtons.OK,MessageBoxIcon.Error);
+					
+				}
+			}
+		}
+
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			this.설정ToolStripMenuItem.Enabled = true;
+			this.plOption.Visible = false;
+			this.txtMessage.Focus();
+		}
+
+		private void btnSender_Click(object sender, EventArgs e)
+		{
+			if (this.txtMessage.Text == "")
+			{
+				this.txtMessage.Focus();
+			}
+			else
+			{
+				Msg_Send();
+			}
+		}
+
+		private void rtbText_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void txtMessage_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			// ENTER 키가 입력되면 작동
+			if(e.KeyChar == (char)13)
+			{
+				if (this.txtMessage.Text == "")
+				{
+					this.txtMessage.Focus();
+				}
+				else
+				{
+					Msg_Send();
+				}
+			}
+		}
+	}
+}
